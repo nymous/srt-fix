a255e6f6379ac7bd0cedf739857b31002781bf1a
Thomas Gaudin <tgaudin@vekia.fr>
2018-02-26 15:32:46 +0100

    Move resync command to its own file

--- index.js
+++ index.js
@@ -1,62 +1,13 @@
 #!/usr/bin/env node
 
-const fs = require('fs')
-const Subtitle = require('subtitle')
+const yargs = require('yargs')
 
-const argv = require('yargs')
-  .usage('$0 <command> [options]')
-  .command('sync', 'resync a given SRT file')
+yargs.usage('$0 <command> [options]')
+  .command(require('./resync'))
   .example('$0 sync -f foo.srt -o fixed.srt -t +8500', 'Resync the SRT file, moving the subs 8.5s later')
-  .option('f', {
-    alias: 'file',
-    demandOption: true,
-    group: 'General',
-    describe: 'Load a file',
-    nargs: 1,
-    type: 'string'
-  })
-  .option('o', {
-    alias: 'output',
-    demandOption: true,
-    group: 'General',
-    describe: 'Output to file',
-    nargs: 1,
-    type: 'string'
-  })
-  .option('d', {
-    alias: 'delay',
-    describe: 'Move subs by X miliseconds forward (+) or backward (-)',
-    group: 'Sync',
-    demandOption: true,
-    nargs: 1,
-    type: 'number'
-  })
-  .option('s', {
-    alias: 'start-at',
-    group: 'Sync',
-    describe: 'Start syncing at this timestamp (format: 01:37:47,512)',
-    nargs: 1,
-    type: 'string',
-    default: '00:00:00,000'
-  })
   .demandCommand(1, 1, 'Use exactly one command', 'Use exactly one command')
   .completion()
   .help('h')
   .alias('h', 'help')
   .wrap(Math.min(120, require('yargs').terminalWidth()))
-  .argv
-
-const inputFile = argv.file
-const outputFile = argv.output
-const syncDelay = argv.delay
-const startSyncTime = Subtitle.toMS(argv['start-at'])
-
-const subFileContent = fs.readFileSync(inputFile, 'utf8')
-const writeStream = fs.createWriteStream(outputFile)
-
-const allSubtitles = Subtitle.parse(subFileContent)
-const correctSubs = allSubtitles.filter(s => s.start <= startSyncTime)
-const subsToFix = allSubtitles.filter(s => s.start > startSyncTime)
-const newSubs = Subtitle.resync(subsToFix, syncDelay)
-
-writeStream.write(Subtitle.stringify([...correctSubs, ...newSubs]))
+  .parse()
---
+++ resync.js
@@ -0,0 +1,72 @@
+const fs = require('fs')
+const Subtitle = require('subtitle')
+
+/**
+ * Sync an SRT file by delaying subtitles by a given amount
+ * @example <caption>This moves subtitles 3.5s sooner after 12 minutes 17 seconds in the movie</caption>
+ * resync({file: 'input.srt', output: 'out.srt', delay: -3500, 'start-at': '00:12:17,000'})
+ * @param {Object} argv - Configuration object
+ * @param {string} argv.file - Input file path, SRT format
+ * @param {string} argv.output - Output file path
+ * @param {number} argv.delay - Delay to apply to subtitles in milliseconds, positive number to make subtitles appear later
+ * @param {string} [argv.start-at] - Start syncing after this timestamp; format: <code>01:27:36,624</code> If omitted it will start at <code>00:00:00,000</code>
+
+ */
+function resync (argv) {
+  const inputFile = argv.file
+  const outputFile = argv.output
+  const syncDelay = argv.delay
+  const startSyncTime = Subtitle.toMS(argv['start-at'])
+
+  console.log(inputFile)
+
+  const subFileContent = fs.readFileSync(inputFile, 'utf8')
+  const writeStream = fs.createWriteStream(outputFile)
+
+  const allSubtitles = Subtitle.parse(subFileContent)
+  const correctSubs = allSubtitles.filter(s => s.start <= startSyncTime)
+  const subsToFix = allSubtitles.filter(s => s.start > startSyncTime)
+  const newSubs = Subtitle.resync(subsToFix, syncDelay)
+
+  writeStream.write(Subtitle.stringify([...correctSubs, ...newSubs]))
+}
+
+module.exports = {
+  command: 'resync',
+  describe: 'resync a given SRT file',
+  builder: {
+    f: {
+      alias: 'file',
+      demandOption: true,
+      group: 'resync',
+      describe: 'Load a file',
+      nargs: 1,
+      type: 'string'
+    },
+    o: {
+      alias: 'output',
+      demandOption: true,
+      group: 'resync',
+      describe: 'Output to file',
+      nargs: 1,
+      type: 'string'
+    },
+    d: {
+      alias: 'delay',
+      demandOption: true,
+      group: 'resync',
+      describe: 'Move subs by X miliseconds forward (+) or backward (-)',
+      nargs: 1,
+      type: 'number'
+    },
+    s: {
+      alias: 'start-at',
+      group: 'resync',
+      describe: 'Start syncing at this timestamp (format: 01:37:47,512)',
+      nargs: 1,
+      type: 'string',
+      default: '00:00:00,000'
+    }
+  },
+  handler: resync
+}
